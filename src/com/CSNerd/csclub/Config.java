fe20c05c3fcea374b824a264c23f71e2805f7864
itsmarsss <itsmarzzzzzz@gmail.com>
2022-10-17 20:36:29 -0400

    Add Config file

---
+++ src/com/CSNerd/csclub/Config.java
@@ -0,0 +1,34 @@
+package com.CSNerd.csclub;
+
+public class Config {
+	private String token;
+	private String serverID;
+	private String prefix;
+	
+	public Config() {}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getServerID() {
+		return serverID;
+	}
+
+	public void setServerID(String serverID) {
+		this.serverID = serverID;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	
+}
