08717935b269a6ade3f1cf68dc2ba275c6d7ab08
itsmarsss <itsmarzzzzzz@gmail.com>
2022-10-17 20:30:30 -0400

    Initial Commit

---
+++ src/com/CSNerd/csclub/CSNerd.java
@@ -0,0 +1,9 @@
+package com.CSNerd.csclub;
+
+public class CSNerd {
+	
+	public CSNerd() {
+		System.out.println("CSNerd");
+	}
+
+}
---
+++ src/com/CSNerd/csclub/Main.java
@@ -0,0 +1,9 @@
+package com.CSNerd.csclub;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CSNerd csn = new CSNerd();
+	}
+
+}
